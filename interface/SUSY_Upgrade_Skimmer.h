70273b1875aee26730b3b4d537f28e0b7ca39674
Basil Schneider <basil.schneider@cern.ch>
2017-03-29 07:17:25 +0200

    Initial commit

---
+++ interface/SUSY_Upgrade_Skimmer.h
@@ -0,0 +1,32 @@
+/*
+ * SUSY_Upgrade_Skimmer.h
+ *
+ *  Created on: 24 Aug 2016
+ *      Author: jkiesele
+ */
+
+#ifndef SUSY_Upgrade_Skimmer_H_
+#define SUSY_Upgrade_Skimmer_H_
+
+#include "interface/basicAnalyzer.h"
+#include "interface/sampleCollection.h"
+#include "classes/DelphesClasses.h"
+
+
+class SUSY_Upgrade_Skimmer: public d_ana::basicAnalyzer{
+public:
+	SUSY_Upgrade_Skimmer():d_ana::basicAnalyzer(){}
+	~SUSY_Upgrade_Skimmer(){}
+
+
+private:
+	void analyze(size_t id);
+
+	void postProcess();
+};
+
+
+
+
+
+#endif /* SUSY_Upgrade_Skimmer_H_ */
