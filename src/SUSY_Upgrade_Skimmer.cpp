e31210d67a0ea92b169982f1079ca3c97443eb83
Basil Schneider <basil.schneider@cern.ch>
2017-05-30 10:12:35 +0200

    Allow events with more than two leptons in skim

--- src/SUSY_Upgrade_Skimmer.cpp
+++ src/SUSY_Upgrade_Skimmer.cpp
@@ -270,8 +270,8 @@ void SUSY_Upgrade_Skimmer::analyze(size_t childid /* this info can be used for p
         }
 
         // Skim
-        if (nLep != 2){ continue; }
-        if (nSoftLep != 2){ continue; }
+        if (nLep < 2){ continue; }
+        if (nSoftLep < 2){ continue; }
         if (!hasSoftSFOS){ continue; }
 
         // Fill electrons
