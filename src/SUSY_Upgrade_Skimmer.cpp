aee11572f4927260865825acdc9e90a7baeaf02b
Basil Schneider <basil.schneider@cern.ch>
2018-01-15 15:55:47 -0600

    Change events for event by event comparison

--- src/SUSY_Upgrade_Skimmer.cpp
+++ src/SUSY_Upgrade_Skimmer.cpp
@@ -607,16 +607,16 @@ void SUSY_Upgrade_Skimmer::analyze(size_t childid /* this info can be used for p
             // The hardcoded numbers are to be used with the following sample:
             // DYJetsToLL_M-5to50_HT-100to200_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_200PU/
             for (size_t i=0; i<genpart.size(); ++i){
-                if ((fabs(genpart.at(i)->PT - 70.666527) < 1.e-6 && fabs(genpart.at(i)->Eta - 1.373891) < 1.e-6) ||
-                    (fabs(genpart.at(i)->PT - 13.970113) < 1.e-6 && fabs(genpart.at(i)->Eta - 2.467708) < 1.e-6) ||
-                    (fabs(genpart.at(i)->PT - 44.967712) < 1.e-6 && fabs(genpart.at(i)->Eta + 1.823656) < 1.e-6) ||
-                    (fabs(genpart.at(i)->PT - 35.736317) < 1.e-6 && fabs(genpart.at(i)->Eta - 2.107728) < 1.e-6) ||
-                    (fabs(genpart.at(i)->PT - 96.810577) < 1.e-6 && fabs(genpart.at(i)->Eta - 1.209147) < 1.e-6) ||
+                if ((fabs(genpart.at(i)->PT - 13.970113) < 1.e-6 && fabs(genpart.at(i)->Eta - 2.467708) < 1.e-6) ||
                     (fabs(genpart.at(i)->PT - 20.706135) < 1.e-6 && fabs(genpart.at(i)->Eta + 2.117317) < 1.e-6) ||
                     (fabs(genpart.at(i)->PT - 19.735157) < 1.e-6 && fabs(genpart.at(i)->Eta - 0.642992) < 1.e-6) ||
                     (fabs(genpart.at(i)->PT - 29.951921) < 1.e-6 && fabs(genpart.at(i)->Eta + 0.918342) < 1.e-6) ||
-                    (fabs(genpart.at(i)->PT - 22.212555) < 1.e-6 && fabs(genpart.at(i)->Eta + 1.916969) < 1.e-6) ||
-                    (fabs(genpart.at(i)->PT - 50.659901) < 1.e-6 && fabs(genpart.at(i)->Eta - 1.615096) < 1.e-6)){
+                    (fabs(genpart.at(i)->PT - 22.212555) < 1.e-6 && fabs(genpart.at(i)->Eta + 9.916969) < 1.e-6) ||
+                    (fabs(genpart.at(i)->PT -  6.281808) < 1.e-6 && fabs(genpart.at(i)->Eta + 0.363280) < 1.e-6) ||
+                    (fabs(genpart.at(i)->PT - 11.641006) < 1.e-6 && fabs(genpart.at(i)->Eta - 0.820936) < 1.e-6) ||
+                    (fabs(genpart.at(i)->PT - 16.675346) < 1.e-6 && fabs(genpart.at(i)->Eta + 1.859797) < 1.e-6) ||
+                    (fabs(genpart.at(i)->PT - 10.828916) < 1.e-6 && fabs(genpart.at(i)->Eta + 1.919281) < 1.e-6) ||
+                    (fabs(genpart.at(i)->PT - 21.267307) < 1.e-6 && fabs(genpart.at(i)->Eta + 0.721485) < 1.e-6)){
                     evtFnd = true;
                     printf("Foo99 Match: Idx: %lu/%lu; ID: %d; Status: %d; pt: %f; eta: %f; phi:%f\n",
                            i+1, genpart.size(), genpart.at(i)->PID, genpart.at(i)->Status, genpart.at(i)->PT, genpart.at(i)->Eta, genpart.at(i)->Phi);
